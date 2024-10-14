f4f23c6484f25c1add6dd978f0634e9be9ec827f
Vova Pereslavtsev <vovapereslavtsev@gmail.com>
2024-10-14 19:19:10 +0300

    added init flyway migration

---
+++ src/main/resources/db/migration/V1__INITIALIZATION_SCHEME.sql
@@ -0,0 +1,27 @@
+CREATE TABLE roles
+(
+    role_id   BIGSERIAL PRIMARY KEY,
+    authority VARCHAR(255)
+);
+
+CREATE TABLE users
+(
+    user_id           BIGSERIAL PRIMARY KEY,
+    username          VARCHAR(255),
+    password          VARCHAR(255),
+    first_name        VARCHAR(255),
+    last_name         VARCHAR(255),
+    email             VARCHAR(255),
+    phone             VARCHAR(255),
+    enabled           BOOLEAN,
+    verification_code INTEGER
+);
+
+CREATE TABLE users_authority
+(
+    role_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (role_id) REFERENCES roles (role_id)
+);
