a356f3906dd8afb0c33d82d5678ef43b429cd099
Vova Pereslavtsev <vovapereslavtsev@gmail.com>
2024-10-15 09:05:54 +0300

    ApplicationUserResponse class has been replaced by record

--- src/main/java/org/example/socialnetwork/dtos/ApplicationUserResponse.java
+++ src/main/java/org/example/socialnetwork/dtos/ApplicationUserResponse.java
@@ -1,12 +1,11 @@
 package org.example.socialnetwork.dtos;
 
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
-public class ApplicationUserResponse {
-    Long id;
-    String firstname;
-    String lastname;
+public record ApplicationUserResponse(
+        Long id,
+        String firstname,
+        String lastname
+) {
 }
