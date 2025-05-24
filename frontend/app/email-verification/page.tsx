6735fb0cb9f692e715618e33aba5ad0c7fc89c3d
Pereslavtsev <162044289+Pereslavtsev1@users.noreply.github.com>
2025-05-24 08:09:38 +0300

    Update page.tsx

--- frontend/app/email-verification/page.tsx
+++ frontend/app/email-verification/page.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import Link from "next/link";
 
 export default function EmailVerificationPage() {
   return (
